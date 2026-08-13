e3dac9aef92610ebc9e5a41999a2cdfd33415e40
taka <taka@gmail.com>
2026-08-13 17:13:57 +0900

    映画データ読み込みを修正

--- script.js
+++ script.js
@@ -40,3 +40,46 @@ movieButtons.forEach(function (movie) {
     moviesVisible = true;
   });
 });
+
+const bestButtons = [
+  { id: "best2022", file: "2022_BEST.txt", list: "bestList2022" },
+  { id: "best2023", file: "2023_BEST.txt", list: "bestList2023" },
+  { id: "best2024", file: "2024_BEST.txt", list: "bestList2024" },
+  { id: "best2025", file: "2025_BEST.txt", list: "bestList2025" }
+];
+
+bestButtons.forEach(function (best) {
+  const button = document.getElementById(best.id);
+  const bestList = document.getElementById(best.list);
+
+  let bestVisible = false;
+
+  button.addEventListener("click", async function () {
+
+    if (bestVisible) {
+      bestList.innerHTML = "";
+      bestVisible = false;
+      return;
+    }
+
+    const response = await fetch(best.file);
+    const text = await response.text();
+
+    bestList.innerHTML = "";
+
+    const lines = text.split("\n");
+
+    lines.forEach(function (line) {
+      if (line.trim() === "") {
+        return;
+      }
+
+      const bestText = document.createElement("p");
+      bestText.textContent = line;
+
+      bestList.appendChild(bestText);
+    });
+
+    bestVisible = true;
+  });
+});
