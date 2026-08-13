8452e5b60c21b968790edd79f5f2af2fafcf37a7
taka <taka@gmail.com>
2026-08-13 16:21:02 +0900

    映画2025のデータを追加

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const movie2025Button = document.getElementById("movie2025");
+const movieList = document.getElementById("movieList");
 
-movie2025Button.addEventListener("click", function () {
-  document.body.innerHTML = `
-    <h1>映画2025</h1>
-    <p>タップできました！</p>
-  `;
+movie2025Button.addEventListener("click", async function () {
+  const response = await fetch("movies2025.txt");
+  const text = await response.text();
+
+  const lines = text.split("\n");
+
+  movieList.innerHTML = "";
+
+  lines.forEach(function (line) {
+    line = line.trim();
+
+    if (!line || line.startsWith("ALL")) {
+      return;
+    }
+
+    const movie = document.createElement("p");
+    movie.textContent = line;
+
+    movieList.appendChild(movie);
+  });
 });
