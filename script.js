97c8e09f885f495cb5d12dee33cd944d0f7860c3
taka <taka@gmail.com>
2026-08-13 16:08:49 +0900

    映画ノートの初期画面

---
+++ script.js
@@ -0,0 +1,8 @@
+const movie2025Button = document.getElementById("movie2025");
+
+movie2025Button.addEventListener("click", function () {
+  document.body.innerHTML = `
+    <h1>映画2025</h1>
+    <p>タップできました！</p>
+  `;
+});
